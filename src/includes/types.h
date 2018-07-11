f71001afbccb5bce9ae0d8f60b7215f44b48422b
Unknown <dedebenui@gmail.com>
2018-07-12 00:08:30 +0900

    Position

--- src/includes/types.h
+++ src/includes/types.h
@@ -3,12 +3,35 @@
 
 enum intersectionType { normal, roundabout };
 
-struct position
-{
-	float x;
-	float y;
-	float z;
+struct Position {
+    float x;
+    float y;
+    float z;
+    Position operator+(const Position& p){
+        Position pos;
+        pos.x = this -> x + p.x;
+        pos.y = this -> y + p.y;
+        pos.y = this -> y + p.y;
+        return pos;
+    }
+    
+    Position operator*(const float& f){
+        Position pos;
+        pos.x = this -> x * f;
+        pos.y = this -> y * f;
+        pos.z = this -> z * f;
+        return pos;
+    }
+    
+    Position operator-(const Position& p){
+        Position pos;
+        pos.x = this -> x - p.x;
+        pos.y = this -> y - p.y;
+        pos.y = this -> y - p.y;
+        return pos;
+    }
 };
+
 struct intersection
 {
 	position pos;
